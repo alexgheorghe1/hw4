67b3314478483a55ecb0b6b20ed3497f96d00bf3
alexgheorghe1 <Alex.gheorghe@kellogg.northwestern.edu>
2022-03-13 21:04:22 +0000

    commit 1 - auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,32 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email = params["email"]
+    entered_password = params["password"]
+
+    #check email
+    @user = User.find_by({email: entered_email}) # use "find_by" when expecting exactly one item
+
+    if @user #yes, email matched; now check the password
+        if BCrypt::Password.new(@user.password) == entered_password
+          session["user_id"] = @user.id
+          flash[:notice] = "Welcome!"
+          redirect_to "/posts" #password matches
+        else #password doesn't match, send back to login page
+          flash[:notice] = "Password is incorrect"
+          redirect_to "/sessions/new" 
+        end
+    else 
+        flash[:notice] = "User name does not exist"
+        redirect_to "/users/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash[:notice] = "You have been logged out"
+    redirect_to "/sessions/new"
   end
+
 end
   
